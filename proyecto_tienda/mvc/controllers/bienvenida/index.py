21a04d5ba5808d81f2e846702f8bdc28da73e365
JesusYael <1721110698@utectulancingo.edu.mx>
2023-02-24 18:24:02 +0000

    tabla productos

--- proyecto_tienda/mvc/controllers/bienvenida/index.py
+++ proyecto_tienda/mvc/controllers/bienvenida/index.py
@@ -1,6 +1,6 @@
 import web
 
-render = web.template.render('mvc/views')
+render = web.template.render('proyecto_tienda/mvc/views')
 class Index:
     def GET(self):
         return render.bienvenida.bienvenida()
